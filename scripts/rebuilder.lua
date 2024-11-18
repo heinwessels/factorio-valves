f3bde87da6a3075f56ab61bd811cffe8a438a5b9
Hein Wessels <heinwessels93@gmail.com>
2024-11-18 21:15:45 +0100

    Fixed that some valves during migrate their connections correctly after the 1.1.1 update

--- scripts/rebuilder.lua
+++ scripts/rebuilder.lua
@@ -3,7 +3,8 @@ local builder = require("__valves__.scripts.builder")
 
 local rebuilder = { }
 
-function rebuilder.rebuild()
+---@param args {full:boolean?}
+function rebuilder.rebuild(args)
     ---@type string[]
     local valve_names = { }
     for valve_name in pairs(constants.valve_names) do
@@ -12,7 +13,31 @@ function rebuilder.rebuild()
 
     for _, surface in pairs(game.surfaces) do
         for _, valve in pairs(surface.find_entities_filtered{name=valve_names}) do
-            builder.destroy(valve)
+
+            if not args.full then
+                builder.destroy(valve)
+            else
+                local name = valve.name
+                local position = valve.position
+                local direction = valve.direction
+                local force = valve.force
+                local mirroring = valve.mirroring -- Technically not needed
+                local marked_for_decon = valve.to_be_deconstructed()
+
+                valve.destroy{raise_destroy = true}
+
+                local new_valve = surface.create_entity{
+                    name = name,
+                    position = position,
+                    direction = direction,
+                    force = force,
+                }
+                assert(new_valve)
+                valve = new_valve
+                if marked_for_decon then valve.order_deconstruction(force) end
+                valve.mirroring = mirroring
+            end
+
             builder.build(valve) -- Will also ensure correct circuit condition
         end
     end
@@ -21,8 +46,8 @@ end
 rebuilder.add_remote_interface = function()
 	remote.add_interface("valves", {
 
-		rebuild_all = function()
-            rebuilder.rebuild()
+		rebuild_all = function(args)
+            rebuilder.rebuild(args)
 		end,
 
     })
