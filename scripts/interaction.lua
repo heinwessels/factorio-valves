6be7af97ff302ce769fecc48225b72cb0d858a65
Hein Wessels <heinwessels93@gmail.com>
2025-04-11 20:48:51 +0200

    More reactive showing of threshold

--- scripts/interaction.lua
+++ scripts/interaction.lua
@@ -1,5 +1,13 @@
 local constants = require("__valves__.constants")
 
+---@class ThresholdRendering
+---@field render_object LuaRenderObject
+---@field default_threshold number
+---@field valve LuaEntity?
+
+---@class PlayerData
+---@field render_threshold ThresholdRendering?
+
 local interaction = {}
 
 ---@param threshold number
@@ -8,12 +16,21 @@ local function format_threshold(threshold)
     return string.format("%d%%", math.floor((threshold * 100) + 0.5))
 end
 
+---@param player_data PlayerData
+local function destroy_render_threshold(player_data)
+    local render_threshold = player_data.render_threshold
+    if render_threshold and render_threshold.render_object then
+        render_threshold.render_object.destroy()
+    end
+end
+
 ---@param event EventData.on_selected_entity_changed
 local function on_selected_entity_changed(event)
     local player = game.get_player(event.player_index) --[[@cast player -? ]]
 
     -- Don't want to set up all the plumbing to create this data.
     -- This is a slow event, so it might be okay for now.
+    ---@type table<number, PlayerData>
     storage.players = storage.players or {}
     local player_data = storage.players[event.player_index]
     if not player_data then
@@ -23,9 +40,7 @@ local function on_selected_entity_changed(event)
 
     -- Always destroy all current render_objects, if any, to keep logic simple.
     -- Regardless of wity is now selected.
-    if player_data.render_threshold and player_data.render_threshold.valid then
-        player_data.render_threshold.destroy()
-    end
+    destroy_render_threshold(player_data)
     player_data.render_threshold = nil
 
     local entity = player.selected
@@ -33,18 +48,23 @@ local function on_selected_entity_changed(event)
     local valve_name = entity.name == "entity-ghost" and entity.ghost_name or entity.name
     if not valve_name then return end
     local valve_type = constants.valve_names[valve_name]
+    if not valve_type then return end
     if valve_type == "one_way" then return end -- Doesn't have a threshold
     local threshold = entity.valve_threshold_override or constants.default_thresholds[valve_type]
 
-    player_data.render_threshold = rendering.draw_text{
-        text = format_threshold(threshold),
-        surface = entity.surface,
-        target = entity,
-        color = {1, 1, 1, 0.8},
-        scale = 1.5,
-        vertical_alignment = "middle",
-        players = { player },
-        alignment = "center",
+    player_data.render_threshold = {
+        valve = entity,
+        default_threshold = constants.default_thresholds[valve_type],
+        render_object = rendering.draw_text{
+            text = format_threshold(threshold),
+            surface = entity.surface,
+            target = entity,
+            color = {1, 1, 1, 0.8},
+            scale = 1.5,
+            vertical_alignment = "middle",
+            players = { player },
+            alignment = "center",
+        }
     }
 end
 
@@ -73,43 +93,35 @@ local function quick_toggle(input, event)
 
     -- Visualize it to the player
     valve.create_build_effect_smoke()
-    storage.players = storage.players or {}
-    local player_data = storage.players[event.player_index]
-    if not player_data then return end
-    if player_data.render_threshold then
-        player_data.render_threshold.text = format_threshold(threshold)
-    end
 end
 
----@param event EventData.on_entity_settings_pasted
-function on_entity_settings_pasted(event)
-    -- Just update the threshold number we show on a valve
-    -- if a player copy pasted settings to it so that it doesn't
-    -- look too weird.
+local function on_tick()
+    -- All we're doing is just updating any threshold rendering that any players
+    -- might have while selecting a valve. This is to handle quick-toggles, copy-pasting,
+    -- multiplayer things, and whetever else might occur. Probably overkill but this function
+    -- will be so fast nobody will be able to measure it.
 
-    local player = game.get_player(event.player_index)
-    if not player then return end
-    local valve = player.selected
-    if not valve then return end
-    local valve_name = valve.name == "entity-ghost" and valve.ghost_name or valve.name
-    if not constants.valve_names[valve_name] then return end
-    local valve_type = constants.valve_names[valve_name]
-    if not valve_type then return end
-    if valve_type == "one_way" then return end -- Doesn't have a threshold
+    for _, player_data in pairs(storage.players or { }) do
+        local render_threshold = player_data.render_threshold
+        if not render_threshold then goto continue end
 
+        local render_object, valve = render_threshold.render_object, render_threshold.valve
+        if not (render_object and render_object.valid and valve and valve.valid) then
+            destroy_render_threshold(player_data)
+            if render_object then render_object.destroy() end
+            player_data.render_threshold = nil
+            goto continue
+        end
 
-    storage.players = storage.players or {}
-    local player_data = storage.players[event.player_index]
-    if not player_data then return end
-    if player_data.render_threshold then
-        local threshold = valve.valve_threshold_override or constants.default_thresholds[valve_type]
-        player_data.render_threshold.text = format_threshold(threshold)
+        render_object.text = format_threshold(valve.valve_threshold_override or render_threshold.default_threshold)
+
+        ::continue::
     end
 end
 
 interaction.events = {
     [defines.events.on_selected_entity_changed] = on_selected_entity_changed,
-    [defines.events.on_entity_settings_pasted] = on_entity_settings_pasted,
+    [defines.events.on_tick] = on_tick,
 }
 for input, custom_input in pairs({
     minus = "valves-threshold-minus",
